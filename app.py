f2d522f52190eb3e24ce40a5f2bb74d6754b79cf
Agustin Ronchi <agustinronchi15@gmail.com>
2025-04-03 16:39:25 -0300

    Conectamos python y datos con mattriz

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-import flask
+from flask import Flask, render_template
+app = Flask(__name__)
 
+turnos = [
+    ["Manuel Perez", "Odontologo", "Martes", "10:00"],
+    ["Marcelo Meza", "Oftalmologo", "Miercoles", "13:00"],
+    ["Nahuel Mendoza", "Otorrino", "Viernes", "12:30"]
+]
 
-import random
+@app.route('/')
+def index():
+    return render_template('index.html', turnos = turnos)
+
+if __name__ == '__main__':
+    app.run(debug = True)
