fe29e4a21b71d9c75f9882bd10673b5f4df17872
Nahuel <nahfernandez@uade.edu.ar>
2025-04-14 22:29:39 -0300

    corrijo detalles

--- Cargadedatos.py
+++ Cargadedatos.py
@@ -2,8 +2,8 @@
 paciente = []
 
 def procesoval(a):
-    if a < 10000000000 or a > 99999999999:
-        print("Su dni debe tener exactamente 11 digitos: ")
+    if a < 10000000 or a > 99999999:
+        print("Su dni debe tener exactamente 8 digitos: ")
         return False
     return True
 
@@ -19,11 +19,11 @@ while proceso != -1:
     while not procesoval(proceso):
         proceso = int(input("Ingrese un dni con 11 digitos o -1 para terminar: "))
 
-    if proceso != -1:
-        paciente.append(proceso)
-        nombyapel = input("Ingrese el nombre y apellido del paciente: ")
-        paciente.append(nombyapel)
-        mail = input("Ingrese el mail del paciente: ")
+    
+    paciente.append(proceso)
+    nombyapel = input("Ingrese el nombre y apellido del paciente: ")
+    paciente.append(nombyapel)
+    mail = input("Ingrese el mail del paciente: ")
 
     while not mailval(mail):
         mail = input("Ingrese un mail valido porfavor: ")
@@ -36,7 +36,7 @@ while proceso != -1:
         paciente.append(part)
 
     proceso = int(input("Ingrese otro DNI o -1 para terminar: "))
-else:
-    print(paciente)
+
+print(paciente)
 
 
