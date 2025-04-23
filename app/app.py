c0cbb0a1ae1c7bdfd303ae93373883eaa20f02a7
Nahuel <nahfernandez@uade.edu.ar>
2025-04-22 23:43:47 -0300

    soluciono redireccionamiento de inicio. agrego lista de obras sociales.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -6,17 +6,17 @@ administra = ['administracion','@altaadminis2025']
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-    return render_template('inicio.html')
-def registro():
     if request.method == 'POST':
         usuario = request.form['usuario']
         contrasenia = request.form['contrasenia']
         if usuario in administra and contrasenia in administra:
-            return render_template('index.html')
+            return redirect(url_for('alta'))
+    return render_template('inicio.html')
 
 @app.route('/alta')
 def alta():
-    return render_template('index.html')
+    obras_sociales = ["OSDE", "Swiss Medical", "Galeno", "Medifé", "Omint", "Sancor Salud", "Federada Salud", "Hospital Italiano", "IOMA", "PAMI", "OSDEPYM", "Unión Personal","Luis Pasteur"]
+    return render_template('index.html', obras_sociales=obras_sociales)
 
 if __name__ == '__main__':
     app.run(debug=True)
