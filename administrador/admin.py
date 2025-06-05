9ea6099d5cba8346dedd8b85fc7401079ed90827
Agustin Ronchi <agustinronchi15@gmail.com>
2025-06-05 14:37:20 -0300

    Comentamos y borramos lo innecesario

--- administrador/admin.py
+++ administrador/admin.py
@@ -7,23 +7,27 @@ paciente = []
 administra = ('admin', 'admin123') #credenciales para iniciar sesión como administrador, como NO se modifica nunca, y NO se debe modificar nunca, estos datos deben estar en una tupla
 usuarios = [] #lista donde se guardan los diccionarios, por supuesto no es una base de datos, solo almacena en ram, entonces cuando se refresca la página la lista se vacía completamente
 
+#Parte inicial de la pagina donde iniciamos sesion como admin o usuario
 @admin_bp.route('/', methods=['GET', 'POST'])
 def inicio():
     mensaje = ''
     mensaje_tipo = ''
     
+    #mensaje que se muestra luego de que un usuario se haya registrado correctamente
     if request.args.get('mensaje') == 'registro_exitoso': 
-        mensaje = '¡Registro exitoso! Ya podés iniciar sesión.' #mensaje que se muestra luego de que un usuario se haya registrado correctamente
+        mensaje = '¡Registro exitoso! Ya podés iniciar sesión.' 
         mensaje_tipo = 'exito'
 
+    #Recibimos los datos del formulario
     if request.method == 'POST': 
         usuario = request.form.get('usuario')
         contrasenia = request.form.get('contrasenia')
 
+        #Cumple con los datos del admin
         if usuario == administra[0] and contrasenia == administra[1]:
             session['usuario'] = 'admin'
             session['rol'] = 'admin'
-            return redirect(url_for('turnos.index')) #solo si se inicia sesión con credenciales de administrador se direcciona a alta.html
+            return redirect(url_for('turnos.index')) #solo si se inicia sesión con credenciales de administrador 
 
         for user in usuarios: #se recorren todos los usuarios y sus contraseñas de todos los diccionarios (uno por usuario) de la lista 'usuarios'
             if user['usuario'] == usuario and user['contrasenia'] == contrasenia:
@@ -36,12 +40,16 @@ def inicio():
 
     return render_template('login.html', mensaje=mensaje, mensaje_tipo=mensaje_tipo)
 
+#En este apartado creamos un nuevo usuario
 @admin_bp.route('/registro', methods=['GET', 'POST'])
 def registro():
+    #lista de obras sociales
     obras_sociales = ["OSDE", "Swiss Medical", "Galeno", "Medifé", "Omint", "Sancor Salud", 
                       "Federada Salud", "Hospital Italiano", "IOMA", "PAMI", "OSDEPYM", 
-                      "Unión Personal", "Luis Pasteur"] #lista de obras sociales
+                      "Unión Personal", "Luis Pasteur"] 
+    
     mensaje = '' #variable de texto vacía para que se le pueda asignar mensaje que le corresponda dependiendo del camino del usuario
+
     if request.method == 'POST':
         nombre = request.form.get('nombre') #acá comienza la captura de los datos que el usuario ingresa en los input. Solicita (request) optener (get) lo que ingresa el usuario para registrarse
         apellido = request.form.get('apellido')
@@ -51,25 +59,33 @@ def registro():
         obra_social = request.form.get('obra_social')
         usuario = request.form.get('usuario')
         contrasenia = request.form.get('contrasenia')
-        if not dni.isdigit() or not (7 <= len(dni) <= 8): #se valida que el formato de DNI sea correcto
+
+        #se valida que el formato de DNI sea correcto
+        if not dni.isdigit() or not (7 <= len(dni) <= 8): 
             mensaje = 'DNI inválido. Debe contener solo números y tener entre 7 y 8 dígitos.'
             return render_template('registro.html', obras_sociales=obras_sociales, mensaje=mensaje)
+        
         email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$' #variable para formato de mail
+
         if not re.match(email_regex, email): #se valida que formato de MAIL sea correcto
             mensaje = 'Email inválido. Ingrese un formato correcto.'
             return render_template('registro.html', obras_sociales=obras_sociales, mensaje=mensaje)
+        
         for user in usuarios: #se valida que el nombre de usuario con el que se quiere registrar el usuario no esté ya en la matriz con los usuarios existentes
             if user['usuario'] == usuario:
                 mensaje = 'El nombre de usuario ya está registrado.'
                 return render_template('registro.html', obras_sociales=obras_sociales, mensaje=mensaje)
+            
         for user in usuarios: #se valida que el dni con el que se quiere registrar el usuario no esté ya en la matriz con los usuarios existentes
             if user['dni'] == dni:
                 mensaje = 'El DNI ya está registrado.'
                 return render_template('registro.html', obras_sociales=obras_sociales, mensaje=mensaje)
+            
         for user in usuarios: #se valida que el mail con el que se quiere registrar el usuario no esté ya en la matriz con los usuarios existentes
             if user['email'] == email:
                 mensaje = 'Este correo electrónico ya está registrado.'
                 return render_template('registro.html', obras_sociales=obras_sociales, mensaje=mensaje)
+            
         usuarios.append({ #se guarda el resgistro de un usuario nuevo
             'nombre': nombre,
             'apellido': apellido,
@@ -94,39 +110,10 @@ def alta():
     return render_template('alta.html', obras_sociales=obras_sociales)
 
 
-@admin_bp.route('/bienvenida') #también pensada como ruta para mostrar funcionalidad, solo iniciando sesión con usuario y contraseña que NO sea de administrador se dirige a esta ruta
-def bienvenida():
-    nombre = request.args.get('nombre')
-    return render_template('usuario_hud.html', nombre=nombre, pacientes=paciente)
-
 @admin_bp.route("/logout")
 def logout():
     session.clear()  # borra todos los datos de sesión
     return redirect(url_for("admin.inicio"))
-
-@admin_bp.route('/turnocliente', methods=['GET', 'POST'])
-def turnocliente():
-
-    obras_sociales = ["OSDE", "Swiss Medical", "Galeno", "Medifé", "Omint", "Sancor Salud", 
-                      "Federada Salud", "Hospital Italiano", "IOMA", "PAMI", "OSDEPYM", 
-                      "Unión Personal", "Luis Pasteur"]
-    
-    if request.method == 'POST':
-        dni = request.form['user_dni']
-        nombre = request.form['user_nombre']
-        obra_social = request.form['tipo']
-        fecha = request.form['user_fecha']
-
-        paciente.append({
-            'dni': dni,
-            'nombre': nombre,
-            'obra_social': obra_social,
-            'fecha': fecha
-        })
-
-        return redirect(url_for('admin.bienvenida'))  # vuelve a misturnos.html
-    return render_template('turnocliente.html', obras_sociales=obras_sociales)
-
 
 @admin_bp.route('/eliminar/<dni>', methods=['GET'])
 def eliminar(dni):
@@ -143,3 +130,31 @@ def eliminar(dni):
 
 def validarDNI(dni):
     return dni.isdigit() and len(dni) == 8
+
+# @admin_bp.route('/bienvenida') #también pensada como ruta para mostrar funcionalidad, solo iniciando sesión con usuario y contraseña que NO sea de administrador se dirige a esta ruta
+# def bienvenida():
+#     nombre = request.args.get('nombre')
+#     return render_template('usuario_hud.html', nombre=nombre, pacientes=paciente)
+
+# @admin_bp.route('/turnocliente', methods=['GET', 'POST'])
+# def turnocliente():
+
+#     obras_sociales = ["OSDE", "Swiss Medical", "Galeno", "Medifé", "Omint", "Sancor Salud", 
+#                       "Federada Salud", "Hospital Italiano", "IOMA", "PAMI", "OSDEPYM", 
+#                       "Unión Personal", "Luis Pasteur"]
+    
+#     if request.method == 'POST':
+#         dni = request.form['user_dni']
+#         nombre = request.form['user_nombre']
+#         obra_social = request.form['tipo']
+#         fecha = request.form['user_fecha']
+
+#         paciente.append({
+#             'dni': dni,
+#             'nombre': nombre,
+#             'obra_social': obra_social,
+#             'fecha': fecha
+#         })
+
+#         return redirect(url_for('admin.bienvenida'))  # vuelve a misturnos.html
+#     return render_template('turnocliente.html', obras_sociales=obras_sociales)
--- turnos/turnos.py
+++ turnos/turnos.py
@@ -35,31 +35,36 @@ def validarMail(email):
 obras_sociales = ["IOMA", "OSDE", "Swiss Medical", "Galeno", "Omint", "Medicus", "Federada Salud", "OSPIP", "PAMI", "OSUTHGRA"]
 especialidades = ["Clínica médica", "Pediatría", "Cardiología", "Dermatología", "Ginecología", "Traumatología", "Neurología"]
 
-# Página principal de turnos
+# Este apartado te dirige a la pagina inicial del admin/usuario desde el login
 @turnos_bp.route("/")
 def index():
     if 'usuario' not in session:
         return redirect(url_for('admin.inicio'))
 
+    # Se encarga de buscar los turnos de JSON, busca el usuario actual y lo guarda, y asigna el rol
     turnos = cargar_turnos()
     usuario_actual = session.get("usuario")
     rol = session.get("rol")
 
+    # Si el usuario no es un admin solo se muestran sus turnos propios
     if rol != 'admin':
         turnos = [t for t in turnos if t["usuario"] == usuario_actual]
         return render_template("usuario_hud.html", turnos=turnos)
+    #En caso de ser admin podra ver todos los turnos disponibles
     else:
         return render_template("admin_hud.html", turnos=turnos)
 
-# Crear nuevo turno
+# Apartado que permite la creacion de un nuevo turno
 @turnos_bp.route("/nuevo", methods=["GET", "POST"])
 def nuevo_turno():
     if 'usuario' not in session:
         return redirect(url_for('admin.inicio'))
     
+    #busca el usuario actual y lo guarda, y asigna el rol
     usuario_actual = session.get("usuario")
     rol = session.get("rol")
 
+    #Al enviar el formulario se obtienen los datos y los asignamos a variables
     if request.method == "POST":
         nombre = request.form["nombre"].strip().capitalize()
         apellido = request.form["apellido"].strip().capitalize()
@@ -70,18 +75,22 @@ def nuevo_turno():
         hora = request.form["hora"]
         especialidad = request.form["especialidad"]
 
+        #Valida el dni y el mail
         if not validarDNI(dni) or not validarMail(email):
             return "Datos inválidos. Volvé a intentarlo."
 
+        #Guardamos en una variable los turnos existentes que estaban en el JSON
         turnos = cargar_turnos()
 
-        # ID incremental simple
+        # Incrementa el DNI en uno (esto se va a cambiar mas adelante)
         nuevo_id = 1
         if turnos:
             nuevo_id = max(t["id"] for t in turnos) + 1
 
+        #chequea quien esta usando el programa
         usuario_actual = session.get("usuario")
 
+        #Guardamos los datos en una biblioteca para mandarlo al JSON
         turno = {
             "id": nuevo_id,
             "nombre": nombre,
@@ -94,16 +103,21 @@ def nuevo_turno():
             "especialidad": especialidad,
             "usuario": usuario_actual
         }
-
+        
+        #Enviamos a JSON
         turnos.append(turno)
         guardar_turnos(turnos)
+        
+        #Volvemos al Hud
         return redirect(url_for("turnos.index"))
 
+    #Si el usuario no es admin lo guardamos para que lo vea como sus turnos
     datos_usuario = {}
     if rol != 'admin':
         from administrador.admin import usuarios
         datos_usuario = next((u for u in usuarios if u['usuario'] == usuario_actual), {})
     
+    #Si es admin mostramos el html para crear turno como admin
     if rol == 'admin':
         return render_template(
             "admin_nuevo.html",
@@ -111,6 +125,7 @@ def nuevo_turno():
             especialidades=especialidades,
             es_admin=True
         )
+    #Si es usuario mostramos el html para crear turno como usuario
     else:
         return render_template(
             "usuario_nuevo.html",
@@ -119,20 +134,23 @@ def nuevo_turno():
             datos_usuario=datos_usuario
         )
 
-# Editar turno
+# Este apartado sirve para editar los turnos existentes
 @turnos_bp.route("/editar/<int:id>", methods=["GET", "POST"])
 def editar(id):
     if 'usuario' not in session:
         return redirect(url_for('admin.inicio'))
 
+    #Cargamos los turnos ya creados y chequeamos el id del seleccionado
     turnos = cargar_turnos()
     turno = next((t for t in turnos if t["id"] == id), None)
     if not turno:
         return "Turno no encontrado"
 
+    #Esto hace que cualquiera que no este registrado en la pagina no pueda ingresar manualmente
     if session.get("rol") != "admin" and turno["usuario"] != session.get("usuario"):
         return "No tenés permiso para editar este turno", 403
 
+    #Reescribimos los datos anteriores
     if request.method == "POST":
         turno["nombre"] = request.form["nombre"].strip().capitalize()
         turno["apellido"] = request.form["apellido"].strip().capitalize()
@@ -144,16 +162,20 @@ def editar(id):
         turno["especialidad"] = request.form["especialidad"]
 
         guardar_turnos(turnos)
+
+        #Volvemos al hud segun seamos admin o usuario
         return redirect(url_for("turnos.index"))
 
+    #Cargamos el html de admin_editar
     return render_template("admin_editar.html", turno=turno, obras_sociales=obras_sociales, especialidades=especialidades)
 
-# Eliminar turno
+# Este es para eliminar turnos
 @turnos_bp.route("/eliminar/<int:id>")
 def eliminar(id):
     if 'usuario' not in session:
         return redirect(url_for('admin.inicio'))
 
+    #Cargamos los turnos y seleccionamos por id
     turnos = cargar_turnos()
     turno = next((t for t in turnos if t["id"] == id), None)
     if not turno:
@@ -167,13 +189,13 @@ def eliminar(id):
     return redirect(url_for("turnos.index"))
 
 # Ver historial del usuario
-@turnos_bp.route("/mis_turnos")
-def mis_turnos():
-    if 'usuario' not in session:
-        return redirect(url_for('admin.inicio'))
+# @turnos_bp.route("/mis_turnos")
+# def mis_turnos():
+#     if 'usuario' not in session:
+#         return redirect(url_for('admin.inicio'))
 
-    usuario_actual = session.get("usuario")
-    turnos = cargar_turnos()
-    mis_turnos = [t for t in turnos if t["usuario"] == usuario_actual]
+#     usuario_actual = session.get("usuario")
+#     turnos = cargar_turnos()
+#     mis_turnos = [t for t in turnos if t["usuario"] == usuario_actual]
 
-    return render_template("usuario_hud.html", turnos=mis_turnos)
+#     return render_template("usuario_hud.html", turnos=mis_turnos)
